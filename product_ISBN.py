fccada08602c26e6dd00da7f5afb54bc806197f5
VinupriyaS <vinu.priya97@gmail.com>
2019-04-18 11:23:53 +0800

    Create product_ISBN.py

---
+++ product_ISBN.py
@@ -0,0 +1,30 @@
+import math
+
+def getISBN(productID):
+    #remove 1st 3 digits from ID
+    ISBN=productID[3:]
+    sum=0
+    for i in range(0,len(ISBN)):
+        sum+=int(ISBN[i])*(10-i)
+        
+ 
+    #find next closest multiple of 11 and subtract sum from this to get errorbit
+    errorbit=(11*(math.ceil(sum/11)) - sum)%11
+    if(errorbit == 10):
+        ISBN=ISBN+"X"
+    else:
+        ISBN=ISBN+str(errorbit)
+    print( "Product ID:",productID,"ISBN:", ISBN)
+
+
+while True:
+    productID=input("Enter productID")
+    if len(productID) !=12 or productID[:3]!="978":
+      print("Invalid ID")
+    else:
+        break
+     
+print(productID)
+getISBN(productID)
+
+
